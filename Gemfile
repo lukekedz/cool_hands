c73a96fb9a66ffbf67fbed983b8fe2931480b034
Luke Kedz <lukekedziora@gmail.com>
2016-12-30 12:16:20 -0500

    revamped gemfile, setup layout

--- Gemfile
+++ Gemfile
@@ -1,12 +1,8 @@
 source 'https://rubygems.org'
 
 gem 'rails', '4.2.0'
-# Use SCSS for stylesheets
-gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
-# Use CoffeeScript for .coffee assets and views
-gem 'coffee-rails', '~> 4.1.0'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
@@ -19,9 +15,14 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # additional gems
 gem 'devise'
 gem 'simple_form'
-gem 'materialize-sass'
 gem 'hirb'
 gem 'pg'
+
+group :development, :test do
+  gem 'spring'
+  gem 'dotenv-rails'
+end
+
 
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160512235720) do
+ActiveRecord::Schema.define(version: 20161229173007) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "months", force: :cascade do |t|
+    t.integer  "year",       null: false
+    t.string   "name",       null: false
+    t.integer  "length",     null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -28,7 +39,7 @@ ActiveRecord::Schema.define(version: 20160512235720) do
     t.datetime "updated_at",                          null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
