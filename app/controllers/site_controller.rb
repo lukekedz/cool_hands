091776be9d836126df22102a935cf1916f168aec
Luke Kedz <lukekedziora@gmail.com>
2016-12-31 13:29:18 -0500

    basic heating working

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -10,25 +10,36 @@ class SiteController < ApplicationController
   end
 
   def practiced
+    saturation = [ "#ffe6e6", "#ffcccc", "#ffb3b3", "#ff9999", "#ff8080", "#ff6666", "#ff4d4d", "#ff3333", "#ff1a1a", "#ff0000" ]
+    color = ""
+
     prev_day = Day.find(params[:block_id].to_i - 1)
 
     streak = 1
     if prev_day.streak != nil
       streak = prev_day.streak.to_i + 1
+      if streak >= 9
+        streak = 9
+      end
+      color = saturation[streak]
     end
 
-    Day.update(params[:block_id], practiced: params[:practiced], streak: streak, minutes: params[:minutes])
+    Day.update(params[:block_id], practiced: params[:practiced], streak: streak, minutes: params[:minutes], color: color)
 
     # TODO: implement streak "clear"
     # TODO: implement robust streak total
     prev_day_id = prev_day.id
     while Day.find(prev_day_id).streak != nil
-      Day.update(prev_day_id, streak: streak)
+      Day.update(prev_day_id, streak: streak, color: color)
       prev_day_id = prev_day_id - 1
     end
 
-    data = {:message => "POST practiced"}
-    render :json => data, :status => :ok
+    # data = {:message => "POST practiced"}
+    # render :json => data, :status => :ok
+
+    # redirect_to root_path
+    # render :js => "window.location = #{root}"
+    render :js => "window.location = '/'"
   end
 
 end
