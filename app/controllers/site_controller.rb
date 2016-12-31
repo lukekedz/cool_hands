deb9a6d2fbed72f9e3a0f0d212294e97c04103b8
Luke Kedz <lukekedziora@gmail.com>
2016-12-31 10:58:27 -0500

    basic streak calculator

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -6,21 +6,26 @@ class SiteController < ApplicationController
     @months = Month.all.reverse
     @current_month = Month.last
     @days   = Day.where(month_id: @current_month.id).order(:id)
+    @week_rows = @days.last.row
   end
 
   def practiced
-    last_day = Day.find(params[:block_id].to_i - 1)
+    prev_day = Day.find(params[:block_id].to_i - 1)
 
     streak = 1
-    if last_day.streak != nil
-      streak = last_day.streak.to_i + 1
+    if prev_day.streak != nil
+      streak = prev_day.streak.to_i + 1
     end
 
-    # increment up and down, reset values to nil, until you get a nil in either direction
-    # increment both directions and all the streak #s should be the same!!!!
-    # this number will be consistent within a steak and help set the color
-
     Day.update(params[:block_id], practiced: params[:practiced], streak: streak, minutes: params[:minutes])
+
+    # TODO: implement streak "clear"
+    # TODO: implement robust streak total
+    prev_day_id = prev_day.id
+    while Day.find(prev_day_id).streak != nil
+      Day.update(prev_day_id, streak: streak)
+      prev_day_id = prev_day_id - 1
+    end
 
     data = {:message => "POST practiced"}
     render :json => data, :status => :ok
