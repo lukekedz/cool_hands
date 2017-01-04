110f4ac117c3b0f923e4554eb576cb93b3cfe368
Luke Kedz <lukekedziora@gmail.com>
2017-01-04 18:43:03 -0500

    upd seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,21 @@
 puts "Seeding DB..."
 puts
 
-User.create!(email: "luke@dev.com", password: "loplop", password_confirmation: "loplop")
-
-# December
-# month = Date.new(2016, 12, 1)
-# month_length = Date.new(2016, 12, -1).day
-# Month.create(name: month.strftime("%B"), length: month_length)
-
 # January
 month = Date.new(2017, 1, 1)
 month_length = Date.new(2017, 1, -1).day
 Month.create(name: month.strftime("%B"), length: month_length)
 
-# February
-# month = Date.new(2017, 2, 1)
-# month_length = Date.new(2017, 2, -1).day
-# Month.create(name: month.strftime("%B"), length: month_length)
+# initial day for edge case on 1/1 streak counting
+Day.create(
+  month_id:  -1,
+  row:       -1,
+  block:     -1,
+  clickable: false,
+  practiced: 0
+)
 
-# March
-# month = Date.new(2017, 3, 1)
-# month_length = Date.new(2017, 3, -1).day
-# Month.create(name: month.strftime("%B"), length: month_length)
-
-row   = 0
+row = 0
 # numbered day of week, ie Sun 0, Mon 1
 block = month.strftime("%w").to_i
 date_day = 1
